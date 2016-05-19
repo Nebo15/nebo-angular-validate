ca2644b8aaa5ba361e1baebfa5d68465c45a3db5
Alexey Bondarenko <alexeybondarenko@me.com>
2016-05-19 13:49:23 +0300

    init bower

---
+++ nebo-angular-validate.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module('nebo-validate', []).provider('$validate', function () {
+
+  var validators = {};
+
+  return {
+    add: function (name, fn) {
+      validators[name] = fn;
+    },
+    $get: function () {
+
+      var obj = {};
+      obj.validator = function (type) {
+        if (!validators[type]) throw new Error('validator not found');
+        return validators[type];
+      };
+
+      return obj;
+    }
+  }
+
+}).directive('validate', function ($validate) {
+
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function (scope, el, attrs, ngModel) {
+
+      if (!ngModel) return;
+      var type;
+
+      attrs.$observe('validate', function (val) {
+        init(val);
+      });
+
+      init(attrs['validate']);
+      function init (newType) {
+
+        if (type) {
+          delete ngModel.$validators[type];
+        }
+        var validatorFn = $validate.validator(newType);
+
+        if (validatorFn) {
+          type = newType;
+          ngModel.$validators[newType] = function (modelValue, viewValue) {
+            console.log('valudate', newType, modelValue);
+            return ngModel.$isEmpty(viewValue) || validatorFn(modelValue);
+          }
+        }
+      }
+    }
+  }
+});
