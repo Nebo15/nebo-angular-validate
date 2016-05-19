2629d39cb188ad36c734f1ae6e1f5cb440c4bff4
Alexey Bondarenko <alexeybondarenko@me.com>
2016-05-19 14:17:20 +0300

    move validation login to the service function

--- nebo-angular-validate.js
+++ nebo-angular-validate.js
@@ -12,8 +12,24 @@ angular.module('nebo-angular-validate', []).provider('$validate', function () {
 
       var obj = {};
       obj.validator = function (type) {
-        if (!validators[type]) throw new Error('validator not found');
+        if (!validators[type]) {
+          console.error('validator '+type+' not found');
+          return null;
+        }
         return validators[type];
+      };
+      obj.validate = function (type, value) {
+        var validateObject = obj.validator(type);
+
+        if (typeof validateObject == 'function') {
+          return validateObject(value);
+        }
+        if (validateObject instanceof RegExp) {
+          return validateObject.test((value || '').toString());
+        }
+        console.error('Unsupported type of validator object', validateObject);
+
+        return false;
       };
 
       return obj;
@@ -40,16 +56,12 @@ angular.module('nebo-angular-validate', []).provider('$validate', function () {
         if (type) {
           delete ngModel.$validators[type];
         }
-        var validatorFn = $validate.validator(newType);
-
-        if (validatorFn) {
+        var validateObj = $validate.validator(newType);
+        if (validateObj) {
           type = newType;
           ngModel.$validators[newType] = function (modelValue, viewValue) {
-            return ngModel.$isEmpty(viewValue) || (
-              typeof validatorFn == 'function' ? validatorFn(modelValue) :
-              validatorFn instanceof RegExp    ? validatorFn.test((modelValue || '').toString()) :
-              false
-            )
+            return ngModel.$isEmpty(viewValue) || $validate.validate(newType, modelValue);
+
           }
         }
       }
